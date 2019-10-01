622274bc3e87f898af395e4962cf6a76eabed287
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-10-01 11:08:50 -0500

    feat: add better fix changes

--- src/Author.java
+++ src/Author.java
@@ -3,10 +3,11 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings("unchecked")
 public class Author extends Person {
-    private List books;
 
+    private List books;
+    
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
         books = new ArrayList();
@@ -19,17 +20,16 @@ public class Author extends Person {
     public List<String> getBooks() {
         return books;
     }
-
     public List<String> publishedBooks() {
         return books;
     }
-
+   
     public void addBook(String book) {
         books.add(book);
     }
 
-    
-    public String sortName() {
+    @Override
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
